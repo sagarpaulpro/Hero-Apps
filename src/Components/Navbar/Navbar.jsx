ee95583b618168277b35e8575d8578cb616e76f4
Sagar Paul <sagarpaul01@yahoo.com>
2025-10-10 15:57:53 +0600

    solving the navbar problem

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,16 +7,9 @@ const Navbar = () => {
         <div>
             <div className="navbar bg-base-100 shadow-sm">
                 <div className="navbar-start">
-                    <div className="dropdown">
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <NavLink to={'/'}>Home</NavLink>
-                            <NavLink to={'/Apps'}>Apps</NavLink>
-                            <NavLink to={'/Installation'}>Installation</NavLink>
-                        </ul>
-                    </div>
                     <NavLink className={'mx-4'} to={'/'}><a className="btn btn-ghost text-xl text-purple-400"> <img className='w-9' src={logo} /> HERO.IO</a></NavLink>
                 </div>
-                <div className="navbar-center hidden lg:flex">
+                <div className="navbar-center lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         <NavLink className={'mx-4'} to={'/'}>Home</NavLink>
                         <NavLink className={'mx-4'} to={'/Apps'}>Apps</NavLink>
